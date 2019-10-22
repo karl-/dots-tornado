5163d9aca0116cce9bc4699437d7c37510a611b2
antoine-unity <antoinebr@unity3d.com>
2019-10-22 16:06:20 -0400

    Jobified camera follow tornado system

--- Assets/DOTS/CameraFollowTornadoSystem.cs
+++ Assets/DOTS/CameraFollowTornadoSystem.cs
@@ -1,15 +1,66 @@
-﻿using Unity.Collections;
+﻿using Unity.Burst;
+using Unity.Collections;
 using Unity.Entities;
+using Unity.Jobs;
 using Unity.Mathematics;
 using Unity.Transforms;
 using UnityEngine;
+using UnityEngine.Jobs;
 
 namespace DotsConversion
 {
     [UpdateAfter(typeof(TornadoMoveSystem))]
-    public sealed class CameraFollowTornadoSystem : ComponentSystem
+    public sealed class CameraFollowTornadoSystem : JobComponentSystem
     {
-        Transform m_MainCamera;
+        [BurstCompile]
+        struct CalculateCameraPosition : IJob
+        {
+            [ReadOnly, DeallocateOnJobCompletion] public NativeArray<Translation> Positions;
+            [ReadOnly] public float3 LookDirection;
+            [WriteOnly] public NativeArray<float3> Result;
+
+            public void Execute()
+            {
+                float3 targetCenter = float3.zero;
+                
+                //Calculate Bounds' center
+                if (Positions.Length != 0)
+                {
+                    float3 pos = Positions[0].Value;
+                    float xMin = pos.x, xMax = pos.x;
+                    float yMin = pos.y, yMax = pos.y;
+                    float zMin = pos.z, zMax = pos.z;
+                    for (int i = 1; i < Positions.Length; ++i)
+                    {
+                        pos = Positions[i].Value;
+                        xMin = math.min(xMin, pos.x);
+                        xMax = math.max(xMax, pos.x);
+                        yMin = math.min(yMin, pos.y);
+                        yMax = math.max(yMax, pos.y);
+                        zMin = math.min(zMin, pos.z);
+                        zMax = math.max(zMax, pos.z);
+                    }
+                    
+                    float3 extents = new float3((xMax - xMin) * 0.5f, (yMax - yMin) * 0.5f, (zMax - zMin) * 0.5f);
+                    targetCenter = new float3(xMin, yMin, zMin) + extents;
+                }
+                
+                //Apply transformation on camera
+                Result[0] = new float3(targetCenter.x, 10f, targetCenter.z) - LookDirection * 60f;
+            }
+        }
+
+        struct ApplyCameraTransform : IJobParallelForTransform
+        {
+            [ReadOnly, DeallocateOnJobCompletion] public NativeArray<float3> CenterResult;
+
+            public void Execute(int index, TransformAccess transform)
+            {
+                transform.position = CenterResult[0];
+            }
+        }
+
+        Transform m_Camera;
         EntityQuery m_TornadoQuery;
 
         protected override void OnCreate()
@@ -17,31 +68,31 @@ namespace DotsConversion
             m_TornadoQuery = GetEntityQuery(typeof(Tornado), typeof(Translation));
         }
 
-        protected override void OnUpdate()
+        protected override JobHandle OnUpdate(JobHandle inputDeps)
         {
-            if (m_MainCamera == null)
-                m_MainCamera = Camera.main.transform;
+            if (m_Camera == null)
+                m_Camera = Camera.main.transform;
 
-            using (var positions = m_TornadoQuery.ToComponentDataArray<Translation>(Allocator.TempJob))
+            NativeArray<float3> ResultBuffer = new NativeArray<float3>(1, Allocator.TempJob);
+
+            var calculateJob = new CalculateCameraPosition
             {
-                var bounds = CalculateTornadoBounds(positions);
-                float3 center = bounds.center;
-                m_MainCamera.position = new Vector3(center.x, 10f, center.z) - m_MainCamera.forward * 60f;
-            }
-        }
+                Positions = m_TornadoQuery.ToComponentDataArray<Translation>(Allocator.TempJob),
+                LookDirection = m_Camera.forward,
+                Result = ResultBuffer,
+            };
+            inputDeps = calculateJob.Schedule(inputDeps);
 
-        Bounds CalculateTornadoBounds(NativeArray<Translation> positions)
-        {
-            if (positions.Length == 0)
-                return new Bounds(Vector3.zero, Vector3.zero);
-
-            Bounds bounds = new Bounds(positions[0].Value, Vector3.zero);
-            for (int i = 1; i < positions.Length; ++i)
+            var transforms = new TransformAccessArray(1);
+            transforms.Add(m_Camera.transform);
+            var applyTransformJob = new ApplyCameraTransform
             {
-                bounds.Encapsulate(positions[i].Value);
-            }
+                CenterResult = ResultBuffer
+            };
 
-            return bounds;
+            var dependency = applyTransformJob.Schedule(transforms, inputDeps);
+            transforms.Dispose();
+            return dependency;
         }
     }
 }

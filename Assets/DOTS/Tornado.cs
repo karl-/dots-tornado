b9a2cb186fdbdc1eefa02f1ad25b51731f5834ef
antoine-unity <antoinebr@unity3d.com>
2019-10-22 11:06:50 -0400

    Added tornado system+component+debug mat

---
+++ Assets/DOTS/Tornado.cs
@@ -0,0 +1,28 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace DotsConversion.Authoring
+{
+    [DisallowMultipleComponent]
+    [RequiresEntityConversion]
+    public sealed class Tornado : MonoBehaviour, IConvertGameObjectToEntity
+    {
+        public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+        {
+            dstManager.AddComponent<DotsConversion.Tornado>(entity);
+            dstManager.SetComponentData(entity, new DotsConversion.Tornado
+            {
+                initialTranslation = transform.position,
+            });
+        }
+    }
+}
+
+namespace DotsConversion
+{
+    public struct Tornado : IComponentData
+    {
+        public float3 initialTranslation;
+    }
+}
---
+++ Assets/DOTS/TornadoMoveSystem.cs
@@ -0,0 +1,37 @@
+﻿using Unity.Burst;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Transforms;
+using Unity.Collections;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace DotsConversion
+{
+    public sealed class TornadoMoveSystem : JobComponentSystem
+    {
+        [BurstCompile]
+        struct MoveJob : IJobForEach<Tornado, Translation>
+        {
+            [ReadOnly] public float Time;
+
+            public void Execute([ReadOnly] ref Tornado tornado, [WriteOnly] ref Translation position)
+            {
+                position.Value = new float3(
+                    tornado.initialTranslation.x + math.cos(Time / 6f) * 30f,
+                    tornado.initialTranslation.y,
+                    tornado.initialTranslation.z + math.cos(Time / 6f * 1.618f) * 30f);
+            }
+        }
+
+        protected override JobHandle OnUpdate(JobHandle inputDeps)
+        {
+            var job = new MoveJob
+            {
+                Time = Time.time,
+            };
+
+            return job.Schedule(this, inputDeps);
+        }
+    }
+}

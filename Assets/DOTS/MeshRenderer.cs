62dae96bfdc742b060d074bdedb5ba7e7f00185c
antoine-unity <antoinebr@unity3d.com>
2019-10-22 17:25:14 -0400

    Added new simple renderer

---
+++ Assets/DOTS/MeshRenderer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Unity.Entities;
+using UnityEngine;
+
+namespace DotsConversion
+{
+    public struct MeshRenderer : ISharedComponentData, IEquatable<MeshRenderer>
+    {
+        public Mesh mesh;
+        public Material material;
+
+        public bool Equals(MeshRenderer other)
+        {
+            return mesh == other.mesh 
+                   && material == other.material;
+        }
+
+        public override int GetHashCode()
+        {
+            int hash = 0;
+            if (!ReferenceEquals(mesh, null)) hash ^= mesh.GetHashCode();
+            if (!ReferenceEquals(material, null)) hash ^= material.GetHashCode();
+            return hash;
+        }
+    }
+}
---
+++ Assets/DOTS/SimpleRenderingSystem.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Transforms;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+using UnityEngine.Profiling;
+
+namespace DotsConversion
+{
+    [UpdateInGroup(typeof(PresentationSystemGroup))]
+    public sealed class SimpleRenderingSystem : ComponentSystem
+    {
+        static readonly List<MeshRenderer> s_UniqueRenderersBuffer = new List<MeshRenderer>();
+        readonly List<Matrix4x4> m_MatricesBuffer = new List<Matrix4x4>(1024);
+        EntityQueryBuilder.F_D<LocalToWorld> m_AddToMatrices;
+        EntityQuery m_MeshQuery;
+
+        protected override void OnCreate()
+        {
+            m_AddToMatrices = AddMatrixToBuffer;
+            m_MeshQuery = GetEntityQuery(typeof(MeshRenderer), typeof(LocalToWorld));
+        }
+
+        protected override void OnUpdate()
+        {
+            s_UniqueRenderersBuffer.Clear();
+            EntityManager.GetAllUniqueSharedComponentData(s_UniqueRenderersBuffer);
+            for (int i = 0; i < s_UniqueRenderersBuffer.Count; ++i)
+            {
+                var renderer = s_UniqueRenderersBuffer[i];
+                if (renderer.mesh == null 
+                    || renderer.material == null 
+                    || !renderer.material.enableInstancing)
+                    continue;
+
+                m_MeshQuery.SetFilter(renderer);
+                m_MatricesBuffer.Clear();
+                Entities.With(m_MeshQuery).ForEach(m_AddToMatrices);
+
+                Graphics.DrawMeshInstanced(renderer.mesh, 0, renderer.material, m_MatricesBuffer);
+            }
+        }
+
+        void AddMatrixToBuffer(ref LocalToWorld matrix)
+        {
+            m_MatricesBuffer.Add(matrix.Value);
+        }
+    }
+}

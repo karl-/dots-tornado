ca6ff1695fadc7b3312f05e2cc41b9fb65f5a301
Jean Blouin <jean@unity3d.com>
2019-10-23 13:25:23 -0400

    modified the bar transform

--- Assets/DOTS/ApplyBarTransform.cs
+++ Assets/DOTS/ApplyBarTransform.cs
@@ -10,28 +10,29 @@ namespace DotsConversion
     {
         struct ApplyTransformJob : IJobForEach<Bar, BarThickness, LocalToWorld>
         {
+            [ReadOnly] public ComponentDataFromEntity<Point> _points;
             public void Execute(
                 [ReadOnly] ref Bar bar,
                 [ReadOnly] ref BarThickness thickness,
                 [WriteOnly] ref LocalToWorld localToWorld)
             {
-//                float3 pointA = bar.a.position;
-//                float3 pointB = bar.b.position;
-//                float dx = pointB.x - pointA.x;
-//                float dy = pointB.y - pointA.y;
-//                float dz = pointB.z - pointA.z;
-//
-//                localToWorld.Value = float4x4.TRS(
-//                    new float3((pointA.x + pointB.x) * .5f, (pointA.y + pointB.y) * .5f, (pointA.z + pointB.z) * .5f),
-//                    quaternion.LookRotation(new float3(dx, dy, dz), math.up()),
-//                    new float3(thickness.thickness, thickness.thickness, bar.a.barLength));/*math.distance(pointA, pointB)*/
+                float3 pointA = _points[bar.a].position;
+                float3 pointB = _points[bar.b].position;
+                float dx = pointB.x - pointA.x;
+                float dy = pointB.y - pointA.y;
+                float dz = pointB.z - pointA.z;
+
+                localToWorld.Value = float4x4.TRS(
+                    new float3((pointA.x + pointB.x) * .5f, (pointA.y + pointB.y) * .5f, (pointA.z + pointB.z) * .5f),
+                    quaternion.LookRotation(new float3(dx, dy, dz), math.up()),
+                    new float3(thickness.thickness, thickness.thickness, bar.barLength));/*math.distance(pointA, pointB)*/
             }
         }
 
         protected override JobHandle OnUpdate(JobHandle inputDeps)
         {
             var job = new ApplyTransformJob();
-
+            job._points = GetComponentDataFromEntity<Point>(true);
             return job.Schedule(this, inputDeps);
         }
     }

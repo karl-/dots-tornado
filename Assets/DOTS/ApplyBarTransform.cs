e059a7a2814b9609c4551cba173eab001354a21f
Karl Henkel <karlh@unity3d.com>
2019-10-24 10:38:39 -0400

    use look rotation safe

--- Assets/DOTS/ApplyBarTransform.cs
+++ Assets/DOTS/ApplyBarTransform.cs
@@ -28,7 +28,7 @@ namespace DotsConversion
 
                 localToWorld.Value = float4x4.TRS(
                     new float3((pointA.x + pointB.x) * .5f, (pointA.y + pointB.y) * .5f, (pointA.z + pointB.z) * .5f),
-                    quaternion.LookRotation(new float3(dx, dy, dz), math.up()),
+                    quaternion.LookRotationSafe(new float3(dx, dy, dz), math.up()),
                     new float3(thickness.thickness, thickness.thickness, bar.barLength));
             }
         }

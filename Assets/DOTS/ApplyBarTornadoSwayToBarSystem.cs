ca84bb3e18929923acec18abdf7987776abf49a9
antoine-unity <antoinebr@unity3d.com>
2019-10-24 11:19:55 -0400

    Jobified Apply Bar Tornado Sway to Bar

--- Assets/DOTS/ApplyBarTornadoSwayToBarSystem.cs
+++ Assets/DOTS/ApplyBarTornadoSwayToBarSystem.cs
@@ -1,92 +1,119 @@
-﻿using DotsConversion;
-using Unity.Burst;
+﻿using Unity.Burst;
 using Unity.Collections;
 using Unity.Entities;
 using Unity.Jobs;
 using Unity.Mathematics;
-using Unity.Transforms;
-using UnityEngine;
-using static Unity.Mathematics.math;
 
-[UpdateAfter(typeof(ApplyTornadoSwayToPointsSystem))]
-public class ApplyBarTornadoSwayToBarSystem : ComponentSystem
+namespace DotsConversion
 {
-    // This declares a new kind of job, which is a unit of work to do.
-    // The job is declared as an IJobForEach<Translation, Rotation>,
-    // meaning it will process all entities in the world that have both
-    // Translation and Rotation components. Change it to process the component
-    // types you want.
-    //
-    // The job is also tagged with the BurstCompile attribute, which means
-    // that the Burst compiler will optimize it for the best performance.
-    //[BurstCompile]
-    public ComponentDataFromEntity<DotsConversion.Point> _points;
-    public ComponentDataFromEntity<DotsConversion.Bar> _bar;
-    public EntityQuery m_Group;
-
-    void ApplyBar(ref DotsConversion.Bar bar)
+    [UpdateAfter(typeof(ApplyTornadoSwayToPointsSystem))]
+    public class ApplyBarTornadoSwayToBarSystem : JobComponentSystem
     {
-        if (bar.a == Entity.Null)
-            return;
-        DotsConversion.Point pA = _points[bar.a];
-        DotsConversion.Point pB = _points[bar.b];
-        float3 pointA = pA.position;
-        float3 pointB = pB.position;
-        float dx = pointB.x - pointA.x;
-        float dy = pointB.y - pointA.y;
-        float dz = pointB.z - pointA.z;
-        // Calculate how much distance has been added due to the tornado force affecting points
-        float dist = math.sqrt(dx * dx + dy * dy + dz * dz);
-        float extraDist = dist - bar.barLength;
-        // When a point is affected by the suction, move it in the direction of the suck by half of the distance traveled
-        float pushX = (dx / dist * extraDist) * .5f;
-        float pushY = (dy / dist * extraDist) * .5f;
-        float pushZ = (dz / dist * extraDist) * .5f;
-       // Debug.Log( "Push " + pushX + " " + pushY + " " + pushZ );
-        if (_points[bar.a].anchor == false && _points[bar.b].anchor == false)
+        [BurstCompile]
+        struct ApplySwayJob : IJob
         {
-            pointA.x += pushX;
-            pointA.y += pushY;
-            pointA.z += pushZ;
-            pointB.x -= pushX;
-            pointB.y -= pushY;
-            pointB.z -= pushZ;
-        }
-        else if (_points[bar.a].anchor)
-        {
-            pointB.x -= pushX * 2f;
-            pointB.y -= pushY * 2f;
-            pointB.z -= pushZ * 2f;
-        }
-        else if (_points[bar.b].anchor)
-        {
-            pointA.x += pushX * 2f;
-            pointA.y += pushY * 2f;
-            pointA.z += pushZ * 2f;
+            [NativeDisableParallelForRestriction] public ComponentDataFromEntity<Point> Points;
+            public NativeArray<Bar> Bars;
+
+            public void Execute()
+            {
+                for (int i = 0; i < Bars.Length; ++i)
+                {
+                    var bar = Bars[i];
+                    if (bar.a == Entity.Null)
+                        return;
+
+                    Point pA = Points[bar.a];
+                    var pB = Points[bar.b];
+                    float3 pointA = pA.position;
+                    float3 pointB = pB.position;
+                    float dx = pointB.x - pointA.x;
+                    float dy = pointB.y - pointA.y;
+                    float dz = pointB.z - pointA.z;
+                    // Calculate how much distance has been added due to the tornado force affecting points
+                    float dist = math.sqrt(dx * dx + dy * dy + dz * dz);
+                    float extraDist = dist - bar.barLength;
+                    // When a point is affected by the suction, move it in the direction of the suck by half of the distance traveled
+                    float pushX = (dx / dist * extraDist) * .5f;
+                    float pushY = (dy / dist * extraDist) * .5f;
+                    float pushZ = (dz / dist * extraDist) * .5f;
+                    // Debug.Log( "Push " + pushX + " " + pushY + " " + pushZ );
+                    if (Points[bar.a].anchor == false && Points[bar.b].anchor == false)
+                    {
+                        pointA.x += pushX;
+                        pointA.y += pushY;
+                        pointA.z += pushZ;
+                        pointB.x -= pushX;
+                        pointB.y -= pushY;
+                        pointB.z -= pushZ;
+                    }
+                    else if (Points[bar.a].anchor)
+                    {
+                        pointB.x -= pushX * 2f;
+                        pointB.y -= pushY * 2f;
+                        pointB.z -= pushZ * 2f;
+                    }
+                    else if (Points[bar.b].anchor)
+                    {
+                        pointA.y += pushY * 2f;
+                        pointA.z += pushZ * 2f;
+                        pointA.x += pushX * 2f;
+                    }
+
+                    pA.position = pointA;
+                    pB.position = pointB;
+
+                    Points[bar.a] = pA;
+                    Points[bar.b] = pB;
+                    bar.extraDist = extraDist;
+                    Bars[i] = bar;
+                }
+            }
         }
 
-        pA.position = pointA;
-        pB.position = pointB;
-        _points[bar.a] = pA;
-        _points[bar.b] = pB;
-        bar.extraDist = extraDist;
-    }
-
-    protected override void OnCreate()
-    {
-        var query = new EntityQueryDesc
+        struct ApplyBarData : IJob
         {
-            All = new ComponentType[] {typeof(DotsConversion.Bar)}
-        };
-        m_Group = GetEntityQuery(query);
-    }
-    
-    //[UpdateAfter(ApplyTornadoSwayToPointsSystem)]
-    protected override void OnUpdate()
-    {
-       // World.GetExistingSystem(typeof(ApplyTornadoSwayToPointsSystem)).RequireForUpdate();
-        _points = GetComponentDataFromEntity<DotsConversion.Point>(false);
-        Entities.With(m_Group).ForEach<DotsConversion.Bar>(ApplyBar);
+            [ReadOnly, DeallocateOnJobCompletion] public NativeArray<Entity> BarEntities;
+            [ReadOnly, DeallocateOnJobCompletion] public NativeArray<Bar> Bars;
+            public EntityCommandBuffer CommandBuffer;
 
+            public void Execute()
+            {
+                for (int i = 0; i < BarEntities.Length; ++i)
+                {
+                    CommandBuffer.SetComponent(BarEntities[i], Bars[i]);
+                }
+            }
+        }
+
+        EntityQuery m_BarsQuery;
+
+        protected override void OnCreate()
+        {
+            m_BarsQuery = GetEntityQuery(typeof(Bar));
+        }
+
+        protected override JobHandle OnUpdate(JobHandle inputDeps)
+        {
+            JobHandle barQueryHandle;
+            var bars = m_BarsQuery.ToComponentDataArray<Bar>(Allocator.TempJob, out barQueryHandle);
+            var swayJob = new ApplySwayJob
+            {
+                Bars = bars,
+                Points = GetComponentDataFromEntity<Point>(),
+            };
+
+            inputDeps = swayJob.Schedule(JobHandle.CombineDependencies(inputDeps, barQueryHandle));
+
+            JobHandle entityQueryHandle;
+            var applyJob = new ApplyBarData
+            {
+                Bars = bars,
+                BarEntities = m_BarsQuery.ToEntityArray(Allocator.TempJob, out entityQueryHandle),
+                CommandBuffer = World.GetExistingSystem<EndSimulationEntityCommandBufferSystem>().CreateCommandBuffer(),
+            };
+
+            return applyJob.Schedule(JobHandle.CombineDependencies(inputDeps, entityQueryHandle));
+        }
     }
 }
